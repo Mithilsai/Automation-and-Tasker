bde6625a3be4d5d026252301ab5fb4230a65c060
Mithil Sai <42375385+Mithilsai@users.noreply.github.com>
2020-08-24 18:55:26 +0530

    Add files via upload

---
+++ pubg_lite_lancher.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+import pyautogui as pg
+import time as ti
+sp.call('C://Program Files (x86)//PUBGLite//Launcher.exe')
+#print(pg.position()) #-- To know the position of the mouse
+ti.sleep(10)
+pg.click(765, 477)
+pg.typewrite("")# entire the password of pubg lite.
+pg.click(923, 594)
